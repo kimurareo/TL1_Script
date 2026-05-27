5054ef8862f076db30432e8bdd5103f121e586e6
kimurareo <k024g0034@g.neec.ac.jp>
2026-05-27 12:57:11 +0900

    Update level_editor.py
    o1-04

--- level_editor.py
+++ level_editor.py
@@ -11,27 +11,65 @@ bl_info = {
     "warning":"",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "object"
+    "category": "Object"
 }
 
-# メニュー項目描画
-def draw_menu_manual(self, context):
-    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
-    #context : カーソルを合わせた時のポップアップのカスタマイズなど使用
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
 
-    #トップバーの「エディターメニュー」に項目（オペレータ）を追加
-    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+    # Blenderクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
 
-#アドオン有効化時コールバック
+    # メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+
+    # 著者表示用の文字列
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    # サブメニューの描画
+    def draw(self, context):
+
+        # トップバーの「エディターメニュー」に項目を追加
+        self.layout.operator(
+            "wm.url_open_preset",
+            text="Manual",
+            icon='HELP'
+        )
+
+# 既存メニューにサブメニュー追加
+def submenu(self, context):
+
+    # ID指定でサブメニューを追加
+    self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# Blenderへ登録するクラス一覧
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
+# アドオン有効化時コールバック
 def register():
-    #メニューに項目を追加
-    bpy.types.TOPBAR_MT_editor_menus.append(draw_menu_manual)
+
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # メニュー追加
+    bpy.types.TOPBAR_MT_editor_menus.append(submenu)
+
     print("レベルエディタが有効化されました。")
 
-#アドオン無効化時コールバック
+# アドオン無効化時コールバック
 def unregister():
-    bpy.types.TOPBAR_MT_editor_menus.remove(draw_menu_manual)
-    print("レベルエディタが無効化されました。")    
+
+    # メニュー削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(submenu)
+
+    # Blenderからクラス削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+    print("レベルエディタが無効化されました。")
 
 # テスト実行用コード
 if __name__ == "__main__":
